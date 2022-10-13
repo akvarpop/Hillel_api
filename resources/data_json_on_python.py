e7268d08b5dc18be4ac35b16695dd0d517e6cf39
Popravka <104668775+akvarpop@users.noreply.github.com>
2022-10-13 13:12:07 +0300

    Update data_json_on_python.py
    jenkins

--- resources/data_json_on_python.py
+++ resources/data_json_on_python.py
@@ -2,16 +2,16 @@ import json
 import pathlib
 from pathlib import Path
 
-# path = Path(pathlib.Path.cwd().parent, "resources", "data.json")
+path = Path(pathlib.Path.cwd(), "data.json")
 
-# with open(path, 'r') as f:
-#     secret_variables = json.load(f)
+with open(path, 'r') as f:
+    secret_variables = json.load(f)
 
-# NAME_ADMIN = secret_variables['name']
-# PASSWORD_ADMIN = secret_variables['password']
-# URL_ENDPOINT = secret_variables['endpoint']
-NAME_ADMIN = "admin"
-PASSWORD_ADMIN = "admin123"
-URL_ENDPOINT = "https://www.aqa.science/"
+NAME_ADMIN = secret_variables['name']
+PASSWORD_ADMIN = secret_variables['password']
+URL_ENDPOINT = secret_variables['endpoint']
+# NAME_ADMIN = "admin"
+# PASSWORD_ADMIN = "admin123"
+# URL_ENDPOINT = "https://www.aqa.science/"
 
 
